8c86c493af5377a0a5e3c40893eb5c9121b62ebf
Dillon Millar <dillon0703@outlook.com>
2025-11-24 11:31:25 +0000

    Added framework for difficulty

---
+++ src/base/Difficulty.java
@@ -0,0 +1,8 @@
+package base;
+
+import rooms.Room;
+
+public class Difficulty {
+    //TODO: implement difficulty with easy medium hard
+    //this should effect, the items that generate, the the room size, and which rooms are added
+}
